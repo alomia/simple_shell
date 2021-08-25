7f99eb4c8e27829abb9996898afd4546f2687f96
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-08-25 15:16:09 -0500

    code compatible with Betty controls

--- readLine.c
+++ readLine.c
@@ -1,31 +1,31 @@
 #include "libhsh.h"
 /**
- * readline - reads a string and returns its value minus one position
+ * readLine - reads a string and returns its value minus one position
  *
  * @prompt: this variable stores a character string
  * Return: command
  */
 char *readLine(char *prompt)
 {
-        char *command = malloc(1 * sizeof(char));
-        size_t size = 0;
-        int len = 0, lenCommand = 0;
 
-        while (prompt[len] != '\0')
-        {
-                len++;
-        }
+char *command = malloc(1 * sizeof(char));
+size_t size = 0;
+int len = 0, lenCommand = 0;
 
-        write(STDOUT_FILENO, prompt, len);
+while (prompt[len] != '\0')
+{
+len++;
+}
 
-        getline(&command, &size, stdin);
+write(STDOUT_FILENO, prompt, len);
+getline(&command, &size, stdin);
 
-        while (command[lenCommand] != '\0')
-        {
-                lenCommand++;
-        }
+while (command[lenCommand] != '\0')
+{
+lenCommand++;
+}
 
-        command[lenCommand - 1] = '\0';
+command[lenCommand - 1] = '\0';
 
-        return (command);
+return (command);
 }
