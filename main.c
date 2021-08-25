01d439eaeecb231e7302c03cd8bc76627fe8dc6f
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-08-25 03:29:02 -0500

    update file: remove two lines of code that prevented correct compilation

--- main.c
+++ main.c
@@ -1,7 +1,4 @@
 #include "libhsh.h"
-#include <unistd.h>
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "UnreachableCode"
 /**
  * main - main function
  *
@@ -20,6 +17,28 @@ int main(void)
                 input = readLine("$ ");
                 
                 command = commandInput(input);
+
+                if (!command[0])
+                {
+                        free(input);
+                        free(command);
+                        continue;
+                }
+
+
+                if (strcmp(command[0], "cd") == 0)
+                {
+                        if (changeDir(command[1]) < 0)
+                        {
+                                perror(command[1]);
+                        }
+                        continue;
+                }
+
+                if (strcmp(command[0], "exit") == 0)
+                {
+                        exit(-1);
+                }
 
                 if (!command[0])
                 {
